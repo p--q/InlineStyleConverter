0be9ff2b73ec10b10ae343ffff250a4ca65933ca
pq <pq.blogspot@gmail.com>
2018-04-06 23:52:38 +0900

    HM

--- InlineStyleConverter/src/inlinestyleconverter.py
+++ InlineStyleConverter/src/inlinestyleconverter.py
@@ -18,45 +18,110 @@ def inlinestyleconverter(htmlfile, pattern=r".*"):  # 正規表現が与えら�
 		except ElementTree.ParseError as e:  # XMLとしてパースできなかったとき。
 			errorLines(e, x)  # エラー部分の出力。
 		parent_map = {c:p for p in root.iter() for c in p if c.tag!="br"}  # 木の、子:親の辞書を作成。brタグはstyle属性のノードとは全く関係ないので除く。
-		getElementXPathIter = xpathiterCreator(parent_map)
+		createStepLists = steplistsCreator(parent_map)
 		styles = set(i.get("style") for i in root.iterfind(style_xpath))  # style属性をもつノードのすべてからstyle属性をすべて取得する。iterfind()は直下以外の子ノードも返る。
 		stylenodedic = {i:root.iterfind('.//*[@style="{}"]'.format(i)) for i in styles}  # キー：sytle属性、値: そのstyle属性のあるノードを返すジェネレーター、の辞書。
-		css = dict()  # キー: sytle属性の値、値: CSSセレクタ
+		cssdic = dict()  # キー: sytle属性の値、値: CSSセレクタ
 		for style, nodeiter in stylenodedic.items():  # 各style属性について。
-			nodes = list(nodeiter)  # このstyle属性のあるノードのリストを取得。
-			if len(nodes)==1:  # ノードの数が1個の時。
-				n = nodes[0]  # このstyle属性のあるノードを取得。
-				for paths in getElementXPathIter(n):  # このノードを選択するロケーションパスのリストを取得。
-					path = "/".join(paths)  # XPathのロケーションパスのリストを/で結合。
-					idsep = "*[@id="
-					if idsep in path:  # idのパスがあるときはidパス以降のみを使用。
-						paths = "{}{}".format(idsep, path.rsplit(idsep, 1)[-1]).split("/")  # id属性のあるノードの子のロケーションパスのリストを取得。"*[@id="で分割して最後の要素を取得して"*[@id="を先頭に追加し直す。
-						xpath = ".//{}".format("/".join(paths))  # XPathにする。
-# 						path = "{}{}".format(idsep, path.rsplit(idsep, 1)[-1])  # id属性のあるノードの子のロケーションパスのリストを取得。"*[@id="で分割して最後の要素を取得して"*[@id="を先頭に追加し直す。
-# 						xpath = ".//{}".format(path)  # ルートノードからのXPathを作成。
+			nodes = set(nodeiter)  # このstyle属性のあるノードの集合を取得。
+			for n in nodes:  # ノードの一つを元にXPathを作成する。
+				steplists = createStepLists(n)  # このノードのロケーションステップのリストのリストを取得。このノードからルート方向のパスの順のリスト。
+				for step in steplists[0]:  # このノードの階層のローケーションパスについて。
+					xpath = ".//{}".format(step)  # XPathの作成。
+					xpathnodes = set(root.findall(xpath))  # 作成したXPathで該当するノードの集合。
+					if nodes==xpathnodes:  # XPathで同じstyle属性をもつノードすべてが取得できたときはこのXPathを採用する。
+						cssdic[style] = xpath
+						break
+				else:  # 1個のロケーションパスではすべてのノードが取得できなかったとき。
+					for steplist in steplists[1:]:  # 上の階層のロケーションパスの一つと組み合わせる。
+						for steps in product(steplist, steplists[0]):
+							xpath = ".//{}//{}".format(*steps)  # XPathの作成。
+							xpathnodes = set(root.findall(xpath))  # 作成したXPathで該当するノードの集合。
+							if nodes==xpathnodes:
+								cssdic[style] = xpath
+								break
+					if style in cssdic:  # XPathが取得されているとき。
+						break
+					else:  # 2つのロケーションパスの組み合わせではすべてのノードを取得できなかったとき。
+						for steps in product(steplists[1], steplists[0]):  # 1つ上の階層のロケーションパスのみとの組み合わせについて。
+							xpath = ".//{}/{}".format(*steps)  # XPathの作成。
+							xpathnodes = set(root.findall(xpath))  # 作成したXPathで該当するノードの集合。
+							if nodes==xpathnodes:
+								cssdic[style] = xpath
+								break		
+					if not style in cssdic:  # まだXPathが取得されていないとき。
+						for steplist in steplists[2:]:  # 3組のロケーションパスのXPathの作成。
+							for steps in product(steplist, steplists[1], steplists[0]): 
+								xpath = ".//{}//{}/{}".format(*steps)  # XPathの作成。				
+								xpathnodes = set(root.findall(xpath))  # 作成したXPathで該当するノードの集合。
+								if nodes==xpathnodes:
+									cssdic[style] = xpath
+									break	
+						if style in cssdic:  # XPathが取得されているとき。
+							break				
+						else:
+							for steps in product(steplist[2], steplists[1], steplists[0]): 
+								xpath = ".//{}/{}/{}".format(*steps)  # XPathの作成。				
+								xpathnodes = set(root.findall(xpath))  # 作成したXPathで該当するノードの集合。
+								if nodes==xpathnodes:
+									cssdic[style] = xpath
+									break						
+					if not style in cssdic:  # まだXPathが取得されていないとき。
+						print("Could not create XPath for {}.".format(style))			
+				
+				
+			
+			
+# 			if len(nodes)==1:  # ノードの数が1個の時。
+# 				n = nodes[0]  # このstyle属性のあるノードを取得。
+# 				for step in generateStep(n):
+# 					xpath = ".//{}".format(step)  # XPathにする。
+# 					xpathnodes = root.findall(xpath)  # 作成したXPathで該当するノードを取得してみる。
+# 					if xpathnodes==1 and xpathnodes[0]==n:
+# 						cssdic[style] = xpath
+# 						break
+# 				else:
+# 					p = parent_map[n]
+					
+					
+				
 
-						xpathnodes = root.findall(xpath)  # 作成したXPathで該当するノードを取得してみる。
-						if n in xpathnodes:  # 目的のノードを取得できたとき。
-							
-							
-							xpathnodes.remove(n)  # 目的のノードを除外。
-							if xpathnodes:  # まだノードが残っている時。目的のノードと同じ階層かを調べる。
-								if parent_map[n] in [parent_map[i] for i in xpathnodes]:  # 各ノードの親ノードが一致するときは同じ階層に他のノードがあるので不適格。(これはありえないはず)
-									print("Could not create the CSS selector to select only one node having\nstyle='{}'".format(style), file=sys.stderr)
-							# CSSパスが短く出来ないか真ん中を削ってみる。
-							# pathsの最初と最後の要素をスペースでつなぐ。
-							 # ダメなら最後の要素だけノードからパターンを再取得してチャレンジ。
-							# ダメなら最初はスペース、最後の2つの要素を>というようにする。	
-									
-							
-							css[style] = paths2CSSOneNode(paths)  # 動作確認したXPathをCSSパスとして採用。		
-							break  # ループをでる。
-						else:  # XPathで元のノードが取得できなかったとき(これはありえないはず)。
-							print("Could not create the CSS selector to select one node having\nstyle='{}'".format(style), file=sys.stderr)		
-					else:  # idのパスがないとき。
-						print("This script does not create a CSS selector as it can not find a node with id attribute up to the root.", file=sys.stderr)
-			else:  # 同じstyleを持つノードが複数ある時。
-				pass
+				
+				
+# 				for paths in getElementXPathIter(n):  # このノードを選択するロケーションステップのリストを取得。
+# 					path = "/".join(paths)  # XPathのロケーションステップを/で結合。
+# 					idsep = "*[@id="
+# 					if idsep in path:  # idのロケーションステップがあるとき
+# 						paths = "{}{}".format(idsep, path.rsplit(idsep, 1)[-1]).split("/")  # idパス以降のロケーションステップを取得。
+# 						
+# 						
+# 						
+# 						xpath = ".//{}".format("/".join(paths))  # XPathにする。
+# # 						path = "{}{}".format(idsep, path.rsplit(idsep, 1)[-1])  # id属性のあるノードの子のロケーションパスのリストを取得。"*[@id="で分割して最後の要素を取得して"*[@id="を先頭に追加し直す。
+# # 						xpath = ".//{}".format(path)  # ルートノードからのXPathを作成。
+# 
+# 						xpathnodes = root.findall(xpath)  # 作成したXPathで該当するノードを取得してみる。
+# 						if n in xpathnodes:  # 目的のノードを取得できたとき。
+# 							
+# 							
+# 							xpathnodes.remove(n)  # 目的のノードを除外。
+# 							if xpathnodes:  # まだノードが残っている時。目的のノードと同じ階層かを調べる。
+# 								if parent_map[n] in [parent_map[i] for i in xpathnodes]:  # 各ノードの親ノードが一致するときは同じ階層に他のノードがあるので不適格。(これはありえないはず)
+# 									print("Could not create the CSS selector to select only one node having\nstyle='{}'".format(style), file=sys.stderr)
+# 							# CSSパスが短く出来ないか真ん中を削ってみる。
+# 							# pathsの最初と最後の要素をスペースでつなぐ。
+# 							 # ダメなら最後の要素だけノードからパターンを再取得してチャレンジ。
+# 							# ダメなら最初はスペース、最後の2つの要素を>というようにする。	
+# 									
+# 							
+# 							css[style] = paths2CSSOneNode(paths)  # 動作確認したXPathをCSSパスとして採用。		
+# 							break  # ループをでる。
+# 						else:  # XPathで元のノードが取得できなかったとき(これはありえないはず)。
+# 							print("Could not create the CSS selector to select one node having\nstyle='{}'".format(style), file=sys.stderr)		
+# 					else:  # idのパスがないとき。
+# 						print("This script does not create a CSS selector as it can not find a node with id attribute up to the root.", file=sys.stderr)
+# 			else:  # 同じstyleを持つノードが複数ある時。
+# 				pass
 		
 		
 		
@@ -68,67 +133,114 @@ def inlinestyleconverter(htmlfile, pattern=r".*"):  # 正規表現が与えら�
 # 			for xpath in xpathiter:
 # 				print("/".join(xpath))
 # 				".//{}".format("/".join(xpath))
-			
-			
-			
-def paths2CSSOneNode(paths):  # paths: ロケーションパスのリスト。
-	csspath = []
-	for path in paths:
-		if "*[@id=" in path:
-			csspath.append(path.replace('*[@id="', "#").split('"')[0])
-		elif "[@class=" in path:
-			csspath.append(".".join(path.split('[@class="')).split('"')[0])
-		else:
-			csspath.append(path)	
-	return " ".join(csspath)  # 1つのノードしか選択しないCSSセレクターを返す。		
+
+# def createStepGenerator(parent_map):			
+# 	def generateStep(node):  # ロケーションステップを返すジェネレーター。
+# 		tag = node.tag  # ノードのタグ。
+# 		idattr = node.get("id")  # id属性の取得。
+# 		if idattr:  # id属性がある時。
+# 			yield '*[@id="{}"]'.format(idattr)  # idのロケーションステップを返す。idの場合はタグは影響しないので*にする。
+# 		yield tag  # タグのみのを返す。
+# 		children = [i for i in list(parent_map[node]) if i.tag==tag]  # 親ノードの子ノードの階層にあるノードのうち同じタグのノードのリストを取得。parent_map[node].iter()だとすべての階層の要素が返ってしまう。
+# 		if len(children)>1:  # 同じタグのノードが同じ階層に複数ある時。
+# 			pathindex = "[{}]".format(children.index(node)+1)  # ノードの順位を取得。1から始まる。
+# 			yield "{}{}".format(tag, pathindex)  # タグに順位をつけたパス。		
+# 		classattr = node.get("class")  # クラス属性の取得。
+# 		if classattr:  # クラス属性がある時。
+# 			classes = classattr.split(" ")  # クラスをリストにする。
+# 			for i in range(1, len(classes)+1):  # 順列の長さ。
+# 				for c in permutations(classes, i):  # 各順列について。
+# 					cls = '[@class="{}"]'.format(" ".join(c))  # クラスのあらゆる組み合わせのパス。
+# 					yield '*{}'.format(cls)  # タグを特定しないクラスを返す。
+# 					yield '{}{}'.format(tag, cls)  # タグを特定したクラスを返す。		
+# 	return generateStep
+# def paths2CSSOneNode(paths):  # paths: ロケーションパスのリスト。
+# 	csspath = []
+# 	for path in paths:
+# 		if "*[@id=" in path:
+# 			csspath.append(path.replace('*[@id="', "#").split('"')[0])
+# 		elif "[@class=" in path:
+# 			csspath.append(".".join(path.split('[@class="')).split('"')[0])
+# 		else:
+# 			csspath.append(path)	
+# 	return " ".join(csspath)  # 1つのノードしか選択しないCSSセレクターを返す。		
 	
 	
 # 	path =[i.replace('*[@id="{', "#").replace('*[@class="{', ".").split("}")[0] for i in paths if "{" in i]
 # 	csspath = ">".join([i.replace('*[@id="', "#").replace('*[@class="', ".").split('"')[0] if "@" in i else i for i in paths])
 			
 		
-	
-			
-
-	
-def xpathiterCreator(parent_map):	
-	pathdic = {}  # 作成したロケーションパスのキャッシュ。	
-	def _getPath(p, n):  # p: 親ノード。n: ノード、からnが取りうるロケーションパスのリストを返す。
-		path = []  # この階層での可能性のあるロケーションパスを入れるリスト。
+def steplistsCreator(parent_map):
+	stepdic = {}  # 作成したロケーションパスのキャッシュ。	
+	def _getStep(n):  # p: 親ノード。n: ノード、からnが取りうるロケーションステップのリストを返す。
+		steplist = []  # この階層での可能性のあるロケーションステップを入れるリスト。
 		tag = n.tag  # ノードのタグ。
 		idattr = n.get("id")  # id属性の取得。
 		if idattr:  # id属性がある時。
-			path.append('*[@id="{}"]'.format(idattr))  # idのパス。idの場合はタグは影響しないので*にする。
+			steplist.append('*[@id="{}"]'.format(idattr))  # idのパス。idの場合はタグは影響しないので*にする。
+		steplist.append(tag)  # タグのみのパス。		
+		children = [i for i in list(parent_map[n]) if i.tag==tag]  # 親ノードの子ノードの階層にあるノードのうち同じタグのノードのリストを取得。p.iter()だとすべての階層の要素が返ってしまう。
+		if len(children)>1:  # 同じタグのノードが同じ階層に複数ある時。
+			tagindex = "[{}]".format(children.index(n)+1)  # ノードの順位を取得。1から始まる。
+			steplist.append("{}{}".format(tag, tagindex))  # タグに順位をつけたパス。			
 		classattr = n.get("class")  # クラス属性の取得。
 		if classattr:  # クラス属性がある時。
 			classes = classattr.split(" ")  # クラスをリストにする。
 			for i in range(1, len(classes)+1):  # 順列の長さ。
 				for c in permutations(classes, i):  # 各順列について。
 					cls = '[@class="{}"]'.format(" ".join(c))  # クラスのあらゆる組み合わせのパス。
-					path.append('{}{}'.format(tag, cls))  # タグを特定する。
-					path.append('*{}'.format(cls))  # タグを特定しない。
-		children = [i for i in list(p) if i.tag==tag]  # 親ノードの子ノードの階層にあるノードのうち同じタグのノードのリストを取得。p.iter()だとすべての階層の要素が返ってしまう。
-		if len(children)>1:  # 同じタグのノードが同じ階層に複数ある時。
-			pathindex = "[{}]".format(children.index(n)+1)  # ノードの順位を取得。1から始まる。
-			path.append("{}{}".format(tag, pathindex))  # タグに順位をつけたパス。		
-		path.append(tag)  # タグのみのパス。			
-		return path	 # id、タグ+class、class、タグ[番号]、タグ、の順にする。
-	def getElementXPathIter(n, r=None):  # ノードのすべてのXPathパターンを返すイテレーターを返す。ロケーションパスのタプルが返る。rは遡る階層数。
-		paths = []  # ロケーションパスのリストを入れるリスト。
-		c = 1  # 階層。1段階から始まる。
+					steplist.append('*{}'.format(cls))  # タグを特定しない。
+					steplist.append('{}{}'.format(tag, cls))  # タグを特定する。
+		return steplist	 # id、タグ、タグ[番号]、class、タグ+class、の順にロケーションステップを返す。
+	def createStepLists(n):  # ノードのすべてのXPathパターンを返すイテレーターを返す。ロケーションステップのタプルが返る。
+		steplists = []  # ロケーションステップのリストを入れるリスト。
 		while n in parent_map:  # 親ノードがあるときのみ実行。
-			p = parent_map[n]  # 親ノードの取得。
-			path = pathdic.setdefault(n, _getPath(p, n))  # nが取りうるロケーションパスのリストを辞書に取得。
-			paths.append(path)  # この階層のロケーションパスのリストを取得。
-			n = p  # 上の階層について調べる。
-			if r is not None:
-				c += 1
-				if c==r:  # 階層番号に達したらwhile文を出る。
-					break
-# 		paths.append(["./"])  # XPathの先頭のロケーションパスをリストで追加。
-		paths.reverse()  # 右に子が来るように逆順にする。
-		return product(*paths)  # pathsの要素のリストのすべての組み合わせを返すイテレーターを返す。
-	return getElementXPathIter
+			steplists.append(stepdic.setdefault(n, _getStep(n)))  # nが取りうるロケーションステップのリストを辞書から取得。
+			n = parent_map[n]
+# 		steplists.reverse()  # 右に子が来るように逆順にする。
+		return steplists
+	return createStepLists
+			
+
+	
+# def xpathiterCreator(parent_map):	
+# 	pathdic = {}  # 作成したロケーションパスのキャッシュ。	
+# 	def _getPath(p, n):  # p: 親ノード。n: ノード、からnが取りうるロケーションパスのリストを返す。
+# 		path = []  # この階層での可能性のあるロケーションパスを入れるリスト。
+# 		tag = n.tag  # ノードのタグ。
+# 		idattr = n.get("id")  # id属性の取得。
+# 		if idattr:  # id属性がある時。
+# 			path.append('*[@id="{}"]'.format(idattr))  # idのパス。idの場合はタグは影響しないので*にする。
+# 		classattr = n.get("class")  # クラス属性の取得。
+# 		if classattr:  # クラス属性がある時。
+# 			classes = classattr.split(" ")  # クラスをリストにする。
+# 			for i in range(1, len(classes)+1):  # 順列の長さ。
+# 				for c in permutations(classes, i):  # 各順列について。
+# 					cls = '[@class="{}"]'.format(" ".join(c))  # クラスのあらゆる組み合わせのパス。
+# 					path.append('{}{}'.format(tag, cls))  # タグを特定する。
+# 					path.append('*{}'.format(cls))  # タグを特定しない。
+# 		children = [i for i in list(p) if i.tag==tag]  # 親ノードの子ノードの階層にあるノードのうち同じタグのノードのリストを取得。p.iter()だとすべての階層の要素が返ってしまう。
+# 		if len(children)>1:  # 同じタグのノードが同じ階層に複数ある時。
+# 			pathindex = "[{}]".format(children.index(n)+1)  # ノードの順位を取得。1から始まる。
+# 			path.append("{}{}".format(tag, pathindex))  # タグに順位をつけたパス。		
+# 		path.append(tag)  # タグのみのパス。			
+# 		return path	 # id、タグ+class、class、タグ[番号]、タグ、の順にする。
+# 	def getElementXPathIter(n, r=None):  # ノードのすべてのXPathパターンを返すイテレーターを返す。ロケーションパスのタプルが返る。rは遡る階層数。
+# 		paths = []  # ロケーションパスのリストを入れるリスト。
+# 		c = 1  # 階層。1段階から始まる。
+# 		while n in parent_map:  # 親ノードがあるときのみ実行。
+# 			p = parent_map[n]  # 親ノードの取得。
+# 			path = pathdic.setdefault(n, _getPath(p, n))  # nが取りうるロケーションパスのリストを辞書に取得。
+# 			paths.append(path)  # この階層のロケーションパスのリストを取得。
+# 			n = p  # 上の階層について調べる。
+# 			if r is not None:
+# 				c += 1
+# 				if c==r:  # 階層番号に達したらwhile文を出る。
+# 					break
+# # 		paths.append(["./"])  # XPathの先頭のロケーションパスをリストで追加。
+# 		paths.reverse()  # 右に子が来るように逆順にする。
+# 		return product(*paths)  # pathsの要素のリストのすべての組み合わせを返すイテレーターを返す。
+# 	return getElementXPathIter
 
 	
 	
